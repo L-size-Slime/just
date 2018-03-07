8cc6b5413706e3a5f2d887935c681458c43f6838
L-size-Slime <373388365@qq.com>
2018-03-07 09:57:24 +0800

    first commit

---
+++ justtest/src/main/java/com/slime/test/justtest/App.java
@@ -0,0 +1,13 @@
+package com.slime.test.justtest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
